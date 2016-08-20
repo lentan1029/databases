a3277e66f648f084f0b86fbf2f8d49ed9994d9f5
lentan1029 <lentan1029@gmail.com>
2016-08-19 19:04:31 -0700

    Connect Client with Controller able to retrieve messages from the database

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,22 +1,24 @@
 var models = require('../models');
 
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
 
 
 module.exports = {
   messages: {
     get: function (req, res) {
+
+      var defaultCorsHeaders = {
+        'Access-Control-Allow-Origin': '*',
+        'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'access-control-allow-headers': 'content-type, accept',
+        'access-control-max-age': 10 // Seconds.
+      };
+
       headers = defaultCorsHeaders;
       headers['Content-Type'] = 'application/json'; 
       res.writeHead(200, headers);
 
       models.messages.get(function(data) {
-        res.end(data);
+        res.end(JSON.stringify(data));
       });
 
     }, // a function which handles a get request for all messages
