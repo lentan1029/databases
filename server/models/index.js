502315303e4b3ad9cb950160cf1cde4000dff1c0
lentan1029 <lentan1029@gmail.com>
2016-08-19 17:05:57 -0700

    Make server add and remove from database when it receives get and post requests

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,64 @@
 var db = require('../db');
+// db.query('SELECT * FROM users', function(err, data) {
+//   console.log(data);
+// });
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      db.query('SELECT * FROM messages', function(err, data) {
+        callback(data);
+      });
+    }, // a function which produces all the messages
+    post: function (message) {
+      var query = 'INSERT into messages (userID,roomID,message,createdAt,updatedAt) \
+        VALUES ( \
+        (SELECT userID from users u where "' + message.username + '"=u.username), \
+        (SELECT roomID from rooms r where "' + message.roomname + '"=r.roomname),"'
+        + message.text + '","' + message.createdAt + '","' + message.updatedAt + '");';
+
+      //messages will have a username property, 
+      db.query(query);
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      db.query('SELECT * from users', function(err, data) {
+        callback(data);
+      });
+    },
+    post: function (username) {
+      var currentUsers = this.get(function(data) {
+        for (var index in data) {
+          if (data[index].username === username) {
+            throw new Error(username, 'is already in the database');
+          }
+        }
+        var query = 'INSERT INTO users (username) VALUES ("' + username + '")';
+        db.query(query);
+      });
+    }
   }
 };
 
+// module.exports.messages.post({
+//   username: 'Brian',
+//   roomname: 'lobby',
+//   text: 'Hey!',
+//   createdAt: 'October 2nd',
+//   updatedAt: '13:54 PM October 3rd'
+// });
+
+// module.exports.messages.get(function(data) {
+//   // console.dir(data);
+//   for (var message in data) {
+//     console.log(data[message]);
+//   }
+// });
+
+module.exports.users.post('lentanasdfass');
+module.exports.users.get(function(data) {
+  console.log(data);
+});
--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users(
+  userID integer NOT NULL AUTO_INCREMENT, username text,
+  PRIMARY KEY (userID)
 );
+
+CREATE TABLE rooms(
+  roomID integer NOT NULL AUTO_INCREMENT, roomname text,
+  PRIMARY KEY (roomID)
+);
+
+CREATE TABLE messages (
+ msgID integer NOT NULL AUTO_INCREMENT, userID integer, roomID integer, createdAt text, updatedAt text, message text, 
+ FOREIGN KEY (userID) REFERENCES users(userID),
+ FOREIGN KEY (roomID) REFERENCES rooms(roomID), PRIMARY KEY (msgID)
+);
+
+
+
+CREATE TABLE rooms_users(
+roomID integer, userID integer,
+FOREIGN KEY (roomID) references rooms(roomID),
+FOREIGN KEY (userID) references users(userID));
 
 /* Create other tables and define schemas for them here! */
 
